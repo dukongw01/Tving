0a8aec713eb4930aa73e4e229bfe8998691cd358
dukongw01 <dukongw01@gmail.com>
2025-11-13 18:11:47 +0900

    20251113

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { createContext, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { HashRouter} from 'react-router-dom'
+import { HashRouter, Routes, Route} from 'react-router-dom'
 
 import Header from './header/Header';
 import Section from './section/Section';
